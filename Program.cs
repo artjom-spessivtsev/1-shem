e86f91034e1481f1b04196ef2fa9219f98fd1bc2
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-11 08:45:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter your favorite football player t-shirt number!");
+
+            string number = Console.ReadLine();
+
+            Console.WriteLine("Sinu number oli:");
+            Console.WriteLine(number);
+        }
+    }
+}
